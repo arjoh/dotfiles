99457bf35554c7ffc0652ed671d26db80647965c
Arjo Hooimeijer <hooimeijer@gmail.com>
2020-07-14 15:57:14 +0200

    Completion and alias for exiftool.

---
+++ oh-my-zsh-custom/plugins/exiftool/exiftool.plugin.zsh
@@ -0,0 +1,2 @@
+# Aliases
+alias exiftool-basic='exiftool -s -Make -Model -Lens -ExposureProgram -FocalLength -Aperture "-ShutterSpeed" -ISO'
--- oh-my-zsh-custom/zshrc/MacBook Pro van Arjo.zsh
+++ oh-my-zsh-custom/zshrc/MacBook Pro van Arjo.zsh
@@ -19,3 +19,8 @@ plugins+=(
   pip
   django
 )
+
+#various
+plugins+=(
+  exiftool
+)
