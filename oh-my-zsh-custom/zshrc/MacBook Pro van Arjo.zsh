0670c2e8ae99b920b07163665abdab426edc1376
Arjo Hooimeijer <hooimeijer@gmail.com>
2020-10-11 23:43:42 +0200

    NodeJS

--- oh-my-zsh-custom/zshrc/MacBook Pro van Arjo.zsh
+++ oh-my-zsh-custom/zshrc/MacBook Pro van Arjo.zsh
@@ -20,7 +20,20 @@ plugins+=(
   django
 )
 
+#node
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && source "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
+
+#angular
+plugins+=(
+  ng
+)
+
 #various
 plugins+=(
   exiftool
 )
+
+alias bundleid='/usr/libexec/PlistBuddy -c "Print CFBundleIdentifier"'
+
