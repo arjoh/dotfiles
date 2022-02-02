3a47509e1199b4f5a16112f379724dca4fc7e74d
Arjo Hooimeijer <hooimeijer@gmail.com>
2022-02-02 18:45:00 +0100

    Update omz & spaceship. Some tools for HI-LAPTOP-AHR.

--- oh-my-zsh-custom/zshrc/HI-LAPTOP-AHR.zsh
+++ oh-my-zsh-custom/zshrc/HI-LAPTOP-AHR.zsh
@@ -3,8 +3,14 @@ plugins+=(
 )
 zstyle :omz:plugins:ssh-agent identities id_rsa
 
-SPACESHIP_DOCKER_SHOW=false
+#SPACESHIP_DOCKER_SHOW=false
 
 umask 022
 
 path+=('/opt/mssql-tools/bin')
+
+export LANG=en_US.UTF-8
+
+sudo ip li set mtu 1200 dev eth0
+
+function gateway() { ip route show default|awk '{print $3}' }
